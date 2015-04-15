b1ad85bf461f20a022cd9b19614cfa47410734c3
Your Name <kulanimakhubele@Kulanis-MacBook-Pro.local>
2015-04-15 13:32:33 +0200

    Drawn a right triangle

--- src/DrawRightTriangle.java
+++ src/DrawRightTriangle.java
@@ -2,4 +2,16 @@
  * Created by kulanimakhubele on 15/04/15.
  */
 public class DrawRightTriangle {
+    public static void main(String[] args){
+
+
+        for (int i = 0; i <= 3; i++ ) {
+            for (int j = 0; j < i; j++) {
+                System.out.print('*');
+            }
+            System.out.println();
+
+
+        }
+    }
 }
